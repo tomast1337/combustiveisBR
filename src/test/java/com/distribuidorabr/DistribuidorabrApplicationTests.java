4daa60eb8818781f9915f74ceb53125aaba63b81
barroscruzc <barroscruzc@gmail.com>
2023-05-10 09:45:07 -0300

    first commit

---
+++ src/test/java/com/distribuidorabr/DistribuidorabrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distribuidorabr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistribuidorabrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
