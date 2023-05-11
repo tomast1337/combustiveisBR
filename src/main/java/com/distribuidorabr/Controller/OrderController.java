49380c2a4233ae2cb954ee7c15911e1ac6196e65
Isaias <99995191+isaias30silva@users.noreply.github.com>
2023-05-11 13:36:37 -0300

    add OrderController, CompanyController and ProductController classes

---
+++ src/main/java/com/distribuidorabr/Controller/OrderController.java
@@ -0,0 +1,41 @@
+package com.distribuidorabr.Controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.distribuidorabr.Model.Order;
+import com.distribuidorabr.Service.interfaces.OrderServiceIntf;
+
+@RestController
+public class OrderController {
+
+	@Autowired
+	private OrderServiceIntf service;
+
+	@GetMapping("/order")
+	public ArrayList<Order> findAll() {
+		return service.findAll();
+	}
+
+	@GetMapping("/order/{codigo}")
+	public ResponseEntity<Order> findById(@PathVariable Integer codigo) {
+		Order res = service.findById(codigo);
+		if (res != null) {
+			return ResponseEntity.ok(res);
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping("/orders")
+	public ResponseEntity<Order> save(@RequestBody Order order) {
+		return ResponseEntity.ok(service.save(order));
+	}
+
+}
