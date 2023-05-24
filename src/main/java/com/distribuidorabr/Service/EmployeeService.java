a84a7cb1f442872d284ae51c6334ed9a889b85bd
barroscruzc <barroscruzc@gmail.com>
2023-05-24 12:08:17 -0300

    Employee - default password auto generated

--- src/main/java/com/distribuidorabr/Service/EmployeeService.java
+++ src/main/java/com/distribuidorabr/Service/EmployeeService.java
@@ -38,6 +38,8 @@ public class EmployeeService implements EmployeeServiceIntf{
 		if(emp.isPresent()) {
 			throw new CpfAlreadyRegisteredException("CPF já cadastrado");
 		} else {
+			//Default Password auto generated
+			employee.setPassword();
 			return dao.save(employee);
 		}
 	}
